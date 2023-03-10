b60804091b31239988a93e85913aaf509c7ce02d
WiseMrMusa <abdulkareemusa01@gmail.com>
2023-03-10 09:57:54 +0100

    Omo

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,37 +1,50 @@
-import BGStyle from "../components/Pattern/BGStyle"
-import { useState , useEffect } from 'react'
-import { useAccount, useContractRead } from "wagmi";
-import StakingABI from "../utils/abi/StakingABI.json"
+import React, { useState, useContext, Suspense } from 'react'
+import { ContractContext } from '..';
+import StakingABI from '../utils/abi/StakingABI.json'
+import { useContractRead } from 'wagmi';
+import { DisplayCard } from '../components/Molecules/DisplayCard';
+import { Link } from 'react-router-dom';
+import { Home } from './Home';
+import { StakeForm } from '../components/Molecules/StakeForm';
+import BGStyle from '../components/Pattern/BGStyle';
+
 
 export const Portfolio = () => {
+    const contract = useContext(ContractContext);
 
-    const { address } = useAccount()
+    const [tokenContractAddrs, setTokenContractAddrs] = useState(["0x5886F287C4473Ce13c58474a261b31c881f8635d"]);
 
-    const [stakedTokens, setStakedTokens] = useState(["0x5886F287C4473Ce13c58474a261b31c881f8635d", "0xF455eB0C273b8E48A0415D34072179Aa25611791", "0x239466949bab2E503830A8e6E1B1Ab45Cd7628a0"]);
-
-    const contractFactory = {
-        old: "0x7c9210c291E00c2030116FcC930D071BBEC5C30f",
-        new: "0xccF6772F52D007E082bF4A01757C4091F5f4dD92"
-    }
-    const CONTRACT = contractFactory.new
-
-
-    const { data: contractData, isError, isLoading, isSuccess } = useContractRead({
-        address: CONTRACT,
+    useContractRead({
+        address: contract,
         abi: StakingABI,
-        functionName: "getStakesByUser",
-        args: [address]
-        // args: ["0x7aEe32638eA48cEE01458bA70317fa37cC2c90aE"]
+        functionName: 'getStakeHolders',
+        onSuccess(data) {
+            setTokenContractAddrs(data)
+        }
     })
 
-    useEffect(() => {
-        setStakedTokens(contractData)
-    }, [contractData])
-
-
     return (
-        <main className=" mt-10 ">
-            Portfolio
+
+        <main className=" mt-10 w-5/6">
+            <BGStyle />
+            <h1 className="text-2xl font-semibold">Dashboard</h1>
+            <div className="grid grid-cols-3 gap-8 mt-10">
+                <Suspense fallback={<StakeForm />}>
+                    {tokenContractAddrs?.map((e, i) => {
+                        return (
+                            <Link key={i} to={`/dashboard/${e}`}>
+                                {/* {e} */}
+                                <DisplayCard key={i} contractAddress={e} />
+                            </Link>
+
+
+                        )
+                    })}
+                </Suspense>
+
+
+            </div>
         </main>
+
     )
 }
